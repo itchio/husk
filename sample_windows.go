6c9b11109835ea6c0966ad7917b1525b5624bd53
Amos Wenger <amoswenger@gmail.com>
2020-05-27 15:42:50 +0200

    Fix Windows sample

--- sample_windows.go
+++ sample_windows.go
@@ -14,7 +14,7 @@ func sample() {
 	link, err := husk.NewShellLink()
 	must(err)
 
-	must(link.Load("C:\Windows\WinSxS\amd64_microsoft-windows-wordpad_31bf3856ad364e35_10.0.18362.267_none_837ed916fe8dbd2f\\Wordpad.lnk"))
+	must(link.Load(`C:\Windows\WinSxS\amd64_microsoft-windows-wordpad_31bf3856ad364e35_10.0.18362.267_none_837ed916fe8dbd2f\Wordpad.lnk`))
 
 	path, err := link.GetPath()
 	must(err)
